c49e4d165f3f6371714a20dd6d8e8a2a4407f0b9
DamirNur <119695906+DamirNur@users.noreply.github.com>
2023-06-05 13:15:29 +0300

    Added welcome screen

--- MovieApp/MovieApp/ViewController.swift
+++ MovieApp/MovieApp/ViewController.swift
@@ -12,8 +12,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.addSubview(welcomeLabel)
     }
-
-
+    
+    private let welcomeLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 20)
+        label.text = "Hello"
+        label.textColor = .white
+        label.frame = CGRect(x: 150, y: 350, width: 50, height: 50)
+        return label
+    }()
 }
 
